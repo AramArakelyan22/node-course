048b6607942ac3eb86a5649a47eb5cac9a4048a7
aram <aram@brainstormtech.io>
2019-04-29 09:56:30 +0400

    JSon Parse

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 const validator = require('validator');
 const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new.',
+    builder: {
+        title: {
+            describe: 'Note define',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove',
+    handler: function(){
+        console.log('Note Removed')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read',
+    handler: function(){
+        console.log('Note Read')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list',
+    handler: function(){
+        console.log('Note Listed')
+    }
+})
+
+yargs.parse();
+/*
 const success = chalk.green.inverse.bold;
 const error = chalk.red.bold;
 
@@ -14,4 +62,5 @@ if(command === 'add') {
 }
 else if (command === 'remove') {
     console.log('Notes Removed')
-}
+}*/
+//console.log(yargs.argv)
--- notes.js
+++ notes.js
@@ -1,5 +1,30 @@
+const fs = require('fs');
+
 const getNotes = () => {
   return "This are notes.."
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  notes.push({
+    title: title,
+    body: body,
+  })
+  console.log(notes);
+}
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const bufferedData = dataBuffer.toString();
+    return JSON.parse(bufferedData);
+  }
+  catch(err) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+}
