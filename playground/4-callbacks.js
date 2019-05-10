522f55a83f7fa184695b13f2aadccdbaa1bfff43
aram <aram@brainstormtech.io>
2019-05-10 10:11:16 +0400

    Handling errors. and creating callbacks

---
+++ playground/4-callbacks.js
@@ -0,0 +1,23 @@
+// setTimeout(() => console.log('TWo seconds'), 2000);
+// const names = ['Aram', 'Artash', 'Sergey'];
+// const shortNames = names.filter(name => name.length <= 4);
+// console.log(shortNames)
+//
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const  data = {
+//             lat: 0,
+//             long: 0,
+//         };
+//         callback (data)
+//     }, 2000)
+//
+// }
+//
+// geocode('Armenia', (date) => console.log(date))
+
+// const add = (num1, num2, callback) => {
+//     setTimeout(() => callback(num1, num2), 2000)
+// }
+// console.log(add(4, 5, () ))
+// TODO Watch the 35 video one more time
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,33 @@
 const request = require('request');
-const API = 'https://api.darksky.net/forecast/db49b2308dc20f5a5b0b0a23f58d435f/40.210306,44.528572';
+const API = 'https://api.darksky.net/forecast/db49b2308dc20f5a5b0b0a23f58d435f/40.210306,44.528572?units=si';
+const geoAPI = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXJhbWFyYWtlbHlhbjIyIiwiYSI6ImNqdmVyeTkzbDF6ZXo0M3J4eHluamtvNXUifQ.nPXWvzgb9liHi1kci0G-dA';
 
-request({url: API}, (error, response) => {
-    console.log(JSON.parse(response.body))
+const weatherRequest = (lan, lon) => request({url: 'https://api.darksky.net/forecast/db49b2308dc20f5a5b0b0a23f58d435f/' + lan + ',' + lon + '?units=si', json: true}, (error, response) => {
+    //console.log(response.body.currently)
+    if(error) {
+        console.log(error)
+    }
+    else if(response.body.error) {
+        console.log('The place is not found')
+    }
+    else {
+        const currentTemp = response.body.currently.temperature;
+        const rainForecast = response.body.currently.precipProbability;
+        console.log(response.body)
+        console.log('It is currently ' + currentTemp + ' degrees out. There is a ' + rainForecast + '% chance of rain.')
+    }
+})
+
+request({url: geoAPI, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location service!')
+    }
+    else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search!')
+    }
+    else {
+        const coordinates = response.body.features[0].geometry.coordinates;
+        console.log(coordinates)
+        weatherRequest(coordinates[1], coordinates[0]);
+    }
 })
