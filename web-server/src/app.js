9fe1364c593a9e22d676782c771b792084a83f37
aram <aram@brainstormtech.io>
2019-05-20 09:59:13 +0400

    styling the app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+
 const app = express();
 
 
 //Define paths
+const publicDirection = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-//Set up static di
-app.use(express.static(path.join(__dirname, '../public')));
 
+// Setup handlers engine and views location
 app.set('views', viewPath);
-app.set('view engine', 'hbs')
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
 
+
+//Set up static directory
+app.use(express.static(publicDirection));
 
 app.get('', (req, res) => {
     res.render('index', {
         name: "Aram",
-        title: "weather app"
+        title: "Weather app"
     })
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help Page',
+        title: 'Help page',
         message: 'We will try to be helpful'
     })
 });
@@ -43,5 +47,21 @@ app.get('/weather', (req, res) => {
         forecast: "Its not hot"
     })
 });
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Aram',
+        errorMessage: 'Help article not found!'
+    })
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Aram',
+        errorMessage: 'Page not found!'
+    })
+})
 
 app.listen(8080, () => {console.log('server started at port 8080')});
