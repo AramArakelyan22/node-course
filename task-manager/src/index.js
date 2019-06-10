f846a3c1c32e9d380f92cfed942f3e6690487fba
aram <aram@brainstormtech.io>
2019-06-10 09:24:51 +0400

    Added Task model

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/Task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,8 +16,19 @@ app.post('/user', (req, resp) => {
        resp.status(200).send(data)
      })
      .catch(err => {
-      resp.status(500).send(err)
+      resp.status(400).send({err})
      })
+})
+
+
+app.post('/task', (req, resp) => {
+    new Task(req.body).save()
+        .then(data => {
+            resp.status(200).send(data)
+        })
+        .catch(err => {
+            resp.status(400).send(err)
+        })
 })
 
 app.listen(port, () => {
