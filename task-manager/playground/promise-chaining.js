73cf0b5a714c69d27f9a441379e508b4532cddad
aram <aram@brainstormtech.io>
2019-06-17 09:55:13 +0400

    Async await finished

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,5 +1,6 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
+const Task = require('../src/models/task');
 
 //5cf4a5e3b2ac451418412d94
 /*
@@ -15,10 +16,20 @@ User.findByIdAndUpdate('5cf4a5e3b2ac451418412d94', {age: 14})
     return err
 })*/
 
+/*
 const userUpdate = async (id, age) => {
     const user = await User.findByIdAndUpdate(id, {age});
     const count = await User.countDocuments({age})
     return count
 }
 userUpdate('5cf4a5e3b2ac451418412d94', 10)
-.then(data => console.log(data));
+.then(data => console.log(data));*/
+
+const findTaskAndDelete = async (id) => {
+    await Task.findByIdAndDelete(id);
+    return await Task.countDocuments();
+
+}
+
+findTaskAndDelete('5cf9fd6eb170e635520e3c95')
+.then(data => console.log(data))
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,57 +10,79 @@ const port = process.env.PORT || 3000;
 //This method will automatically parse the incoming data
 app.use(express.json())
 
-app.post('/user', (req, resp) => {
- new User(req.body).save()
-     .then(data => {
-       resp.status(200).send(data)
-     })
-     .catch(err => {
-      resp.status(400).send({err})
-     })
+app.post('/user', async (req, resp) => {
+ const newUser =  new User(req.body);
+
+ try{
+     await newUser.save();
+     resp.status(200).send(newUser)
+ }
+ catch(er) {
+    resp.status(400).send(er);
+ }
 })
 
 
-app.post('/task', (req, resp) => {
-    new Task(req.body).save()
-        .then(data => {
-            resp.status(200).send(data)
-        })
-        .catch(err => {
-            resp.status(400).send(err)
-        })
+app.post('/task', async (req, resp) => {
+   const task =  new Task(req.body);
+    try {
+      await task.save();
+       resp.status(200).send(task)
+    }
+    catch(e) {
+        resp.status(400).send(e)
+    }
 })
 
-app.get('/users', (req, resp) => {
-    User.find()
-        .then(data => resp.status(200).send(data))
-        .catch(err => resp.status(500).send(err))
+app.get('/users', async (req, resp) => {
+    try {
+        const users = await User.find();
+        resp.status(200).send(users)
+    }
+    catch(er) {
+        resp.status(400).send(er)
+    }
 })
 
-app.get('/users/:id', (req, resp) => {
-    const _id = req.params.id;
-    User.findById( _id)
-        .then(data => {
-            console.log(data);
-            if(!data) {
-                resp.status(404).send('User did not find!')
-            }
-            resp.status(200).send(data)
-        })
-        .catch(err => resp.status(500).send(err))
+app.get('/users/:id', async (req, resp) => {
+    try {
+        const _id = req.params.id;
+        const user = await User.findById(_id);
+        if(!user) {
+           return resp.status(404).send()
+        }
+        resp.status(200).send(user);
+    }
+    catch(e) {
+        resp.status(400).send(e)
+    }
 })
 
-app.get('/tasks', (req, resp) => {
-    Task.find()
-        .then(tasks => resp.status(200).send(tasks))
-        .catch(err => resp.status(500).send(err))
+app.get('/tasks', async (req, resp) => {
+    try {
+        const tasks = await Task.find();
+        resp.status(200).send(tasks);
+    }
+    catch(e) {
+        resp.status(400).send(e)
+    }
+
 })
 
-app.get('/tasks/:id', (req, resp) => {
+app.get('/tasks/:id', async (req, resp) => {
     const id = req.params.id;
-    Task.findById(id)
-        .then(tasks => resp.status(200).send(tasks))
-        .catch(err => resp.status(500).send(err))
+    try {
+        const task = await Task.findById(id);
+        if(!task) {
+            return resp.status(404).send()
+        }
+        resp.status(200).send(task)
+    }
+    catch(e) {
+        resp.status(400).send(e)
+    }
+
+
 })
 
 app.listen(port, () => {
